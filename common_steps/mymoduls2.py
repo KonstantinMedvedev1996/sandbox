abc0aeb03292da79adb42c3326e9263867d58dc0
KonstantinMedvedev1996 <konstantin.medvedev.1996@gmail.com>
2023-05-06 13:27:48 +0300

    seaborn_continue

---
+++ common_steps/mymoduls2.py
@@ -0,0 +1,12 @@
+def hello():
+    print('Hello, world!')
+
+def fib(n):
+    a = b = 1
+    for i in range(n - 2):
+        a, b = b, a + b
+    return b
+
+hello()
+for i in range(10):
+    print(fib(i))
---
+++ common_steps/programm.py
@@ -0,0 +1,5 @@
+import mymoduls_k
+
+mymoduls_k.hello()
+
+print(mymoduls_k.fib(15))
