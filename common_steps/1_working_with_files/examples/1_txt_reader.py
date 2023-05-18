79f59803105f2d1d6be6926a7bc3e35aca7b7378
KonstantinMedvedev1996 <konstantin.medvedev.1996@gmail.com>
2023-05-18 10:01:29 +0300

    working_with _files

--- common_steps/1_working_with_files/examples/1_txt_reader.py
+++ common_steps/1_working_with_files/examples/1_txt_reader.py
@@ -1,15 +1,21 @@
-from files import TXT_FILE_PATH
+# from files import TXT_FILE_PATH
+
+# TXT_FILE_PATH = '../files/example.txt'
+
+TXT_FILE_PATH = "example.txt"
 
 some_file = open(TXT_FILE_PATH, "r")
 
+# some_file = open("example.txt", "r")
+
 # Read the exact bites amount
-print(some_file.read(7))
+# print(some_file.read(7))
 
 # Read a single line
-print(some_file.readline())
+# print(some_file.readline())
 
 # Get all lines as list
-print(some_file.readlines(), "\n")
+# print(some_file.readlines(), "\n")
 
 # Read from current cursor position till the end
 print(some_file.read())
